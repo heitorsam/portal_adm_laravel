b61c913ec53dc4efc246ea2288b4bcaaad1e7fb4
Heitor Scalabrini <47569622+heitorsam@users.noreply.github.com>
2024-07-05 09:53:49 -0300

    laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,69 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::resource('produtos', ProdutoController::Class);
+
+
+
+
+
+
+
+
+/*
+Route::get('/empresa', function(){
+    return view('site/empresa');
+});
+*/
+
+Route::view('/empresa', 'site/empresa');
+
+Route::view('/info', 'site/info');
+
+Route::any('/any', function(){
+    return "Permite todo acesso http";
+});
+
+Route::match(['get','post'],'/match', function(){
+    return "Permite dados definidos";
+});
+
+Route::get('/produto/{id}/{categoria?}', function($id, $categoria = 'Não Definida'){
+
+    return "O id do produto é: ". $id . " e a categoria é: " . $categoria;
+
+});
+
+Route::redirect('/sobre', '/empresa');
+
+Route::get('/news', function(){
+    return view('site/news');
+})->name('noticias');
+
+Route::get('/novidades', function(){
+    return redirect()->route('noticias');
+});
+
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.'
+], function(){
+
+    Route::get('cliente', function () {
+        return view('admin.cliente');
+    })->name('cliente');
+
+    Route::get('dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+    
+    Route::get('user', function () {
+        return view('admin.user');
+    })->name('user');
+
 });
